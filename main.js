f52b632d722a331ab45061c3c7dd2df70d240b87
Mathias <math92k4@stud.kea.dk>
2021-04-26 15:22:02 +0200

    content fetches

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-import './sass_folder/style.scss'; 
+import "./sass_folder/style.scss";
 
+import { getJSON } from "./rest_actions.js";
+import { post } from "./rest_actions.js";
 
-import { get } from "./rest_actions.js"; 
-import { post } from "./rest_actions.js"; 
+window.addEventListener("load", init);
 
-get("https://frontendspring2021-a6f0.restdb.io/rest/games"); 
+async function init() {
+  const games = await getJSON("https://frontendspring2021-a6f0.restdb.io/rest/games");
+  const types = await getJSON("https://frontendspring2021-a6f0.restdb.io/rest/types");
+  const areas = await getJSON("https://frontendspring2021-a6f0.restdb.io/rest/areas");
 
+  const ctaBtn = document.querySelector("#cta_btn");
+  ctaBtn.addEventListener("click", function () {
+    document.querySelector("form").classList.toggle("open");
+  });
+
+  setUpForm();
+}
+
+function setUpForm() {
+  console.log("soFarSoGood");
+}

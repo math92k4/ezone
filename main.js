587f7c9a94510870afececb3a369946f287faf64
Mathias <math92k4@stud.kea.dk>
2021-04-26 15:55:24 +0200

    init loading screen

--- main.js
+++ main.js
@@ -6,9 +6,18 @@ import { post } from "./rest_actions.js";
 window.addEventListener("load", init);
 
 async function init() {
+  const allBtns = document.querySelectorAll("button");
+  allBtns.forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      event.preventDefault();
+    });
+  });
+
   const games = await getJSON("https://frontendspring2021-a6f0.restdb.io/rest/games");
   const types = await getJSON("https://frontendspring2021-a6f0.restdb.io/rest/types");
   const areas = await getJSON("https://frontendspring2021-a6f0.restdb.io/rest/areas");
+
+  document.querySelector("#loading_screen").classList.add("loaded");
 
   const ctaBtn = document.querySelector("#cta_btn");
   ctaBtn.addEventListener("click", function () {
